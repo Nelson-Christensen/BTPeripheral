ef1cf9d1b77cac6eceafd62919ac6f666fe11a16
Nelson Christensen <Nelson.Christensen@interactivesolutions.se>
2022-03-02 16:25:24 +0100

    finished up bt peripheral listening

--- BTPeripheral/main.swift
+++ BTPeripheral/main.swift
@@ -6,6 +6,251 @@
 //
 
 import Foundation
+import CoreBluetooth
+import AppKit
 
-print("Hello, World!")
+struct TransferService {
+    static let serviceUUID = CBUUID(string: "E20A39F4-73F5-4BC4-A12F-17D1AD07A961")
+    static let characteristicUUID = CBUUID(string: "08590F7E-DB05-467E-8757-72F6FAEB13D4")
+}
 
+
+print("1")
+class Peripheral: NSObject, CBPeripheralManagerDelegate
+{
+    var peripheralManager : CBPeripheralManager!
+    
+    var transferCharacteristic: CBMutableCharacteristic?
+    var connectedCentral: CBCentral?
+    var dataToSend = Data()
+    var sendDataIndex: Int = 0
+    
+    override init(){
+        super.init()
+//        super.init(delegate: self, queue: nil, options: [CBPeripheralManagerOptionShowPowerAlertKey: true])
+        print("1.75")
+        peripheralManager = CBPeripheralManager(delegate: self, queue: nil, options: [CBPeripheralManagerOptionShowPowerAlertKey: true])
+        if (peripheralManager.state == CBManagerState.poweredOn){
+            print("Powered on")
+        } else {
+            print("Not powered on")
+        }
+    }
+    
+    private func setupPeripheral() {
+        // Build our service.
+        print("Setting up peripheral")
+        
+        // Start with the CBMutableCharacteristic.
+        let transferCharacteristic = CBMutableCharacteristic(type: TransferService.characteristicUUID,
+                                                         properties: [.notify, .writeWithoutResponse],
+                                                         value: nil,
+                                                         permissions: [.readable, .writeable])
+        
+        // Create a service from the characteristic.
+        let transferService = CBMutableService(type: TransferService.serviceUUID, primary: true)
+        
+        // Add the characteristic to the service.
+        transferService.characteristics = [transferCharacteristic]
+        
+        // And add it to the peripheral manager.
+        peripheralManager.add(transferService)
+        
+        // Save the characteristic for later.
+        self.transferCharacteristic = transferCharacteristic
+        
+        peripheralManager.startAdvertising([CBAdvertisementDataLocalNameKey : "My Peripheral", CBAdvertisementDataServiceUUIDsKey: [TransferService.serviceUUID]])
+    }
+
+    
+    func peripheralManagerDidUpdateState(_ peripheral: CBPeripheralManager)
+    {
+            switch peripheral.state {
+            case .poweredOn:
+                // ... so start working with the peripheral
+                print("CBManager is powered on")
+                setupPeripheral()
+            case .poweredOff:
+                print("CBManager is not powered on")
+                // In a real app, you'd deal with all the states accordingly
+                return
+            case .resetting:
+                print("CBManager is resetting")
+                // In a real app, you'd deal with all the states accordingly
+                return
+            case .unauthorized:
+                // In a real app, you'd deal with all the states accordingly
+                if #available(iOS 13.0, *) {
+                    switch peripheral.authorization {
+                    case .denied:
+                        print("You are not authorized to use Bluetooth")
+                    case .restricted:
+                        print("Bluetooth is restricted")
+                    default:
+                        print("Unexpected authorization")
+                    }
+                } else {
+                    // Fallback on earlier versions
+                }
+                return
+            case .unknown:
+                print("CBManager state is unknown")
+                // In a real app, you'd deal with all the states accordingly
+                return
+            case .unsupported:
+                print("Bluetooth is not supported on this device")
+                // In a real app, you'd deal with all the states accordingly
+                return
+            @unknown default:
+                print("A previously unknown peripheral manager state occurred")
+                // In a real app, you'd deal with yet unknown cases that might occur in the future
+                return
+            }
+
+    }
+
+    func peripheralManagerDidStartAdvertising(_ peripheral: CBPeripheralManager, error: NSError?)
+    {
+        print("Did start advertising")
+        if let error = error
+        {
+            print("Failed due to error: \(error)")
+            return
+        }
+        print("Success")
+    }
+    
+    
+    /*
+     *  Sends the next amount of data to the connected central
+     */
+    static var sendingEOM = false
+    
+    private func sendData() {
+        
+        guard let transferCharacteristic = transferCharacteristic else {
+            return
+        }
+        
+        // First up, check if we're meant to be sending an EOM
+        if Peripheral.sendingEOM {
+            // send it
+            let didSend = peripheralManager.updateValue("EOM".data(using: .utf8)!, for: transferCharacteristic, onSubscribedCentrals: nil)
+            // Did it send?
+            if didSend {
+                // It did, so mark it as sent
+                Peripheral.sendingEOM = false
+                print("Sent: EOM")
+            }
+            // It didn't send, so we'll exit and wait for peripheralManagerIsReadyToUpdateSubscribers to call sendData again
+            return
+        }
+        
+        // We're not sending an EOM, so we're sending data
+        // Is there any left to send?
+        if sendDataIndex >= dataToSend.count {
+            // No data left.  Do nothing
+            return
+        }
+        
+        // There's data left, so send until the callback fails, or we're done.
+        var didSend = true
+        while didSend {
+            
+            // Work out how big it should be
+            var amountToSend = dataToSend.count - sendDataIndex
+            if let mtu = connectedCentral?.maximumUpdateValueLength {
+                amountToSend = min(amountToSend, mtu)
+            }
+            
+            // Copy out the data we want
+            let chunk = dataToSend.subdata(in: sendDataIndex..<(sendDataIndex + amountToSend))
+            
+            // Send it
+            didSend = peripheralManager.updateValue(chunk, for: transferCharacteristic, onSubscribedCentrals: nil)
+            
+            // If it didn't work, drop out and wait for the callback
+            if !didSend {
+                return
+            }
+            
+            let stringFromData = String(data: chunk, encoding: .utf8)
+            print("Sent %d bytes: %s", chunk.count, String(describing: stringFromData))
+            
+            // It did send, so update our index
+            sendDataIndex += amountToSend
+            // Was it the last one?
+            if sendDataIndex >= dataToSend.count {
+                // It was - send an EOM
+                
+                // Set this so if the send fails, we'll send it next time
+                Peripheral.sendingEOM = true
+                
+                //Send it
+                let eomSent = peripheralManager.updateValue("EOM".data(using: .utf8)!,
+                                                             for: transferCharacteristic, onSubscribedCentrals: nil)
+                
+                if eomSent {
+                    // It sent; we're all done
+                    Peripheral.sendingEOM = false
+                    print("Sent: EOM")
+                }
+                return
+            }
+        }
+    }
+    
+    /*
+     *  Catch when someone subscribes to our characteristic, then start sending them data
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didSubscribeTo characteristic: CBCharacteristic) {
+        print("Central subscribed to characteristic")
+        
+        // Get the data
+        dataToSend = "Hejsan tillbaka :D".data(using: .utf8)!
+        
+        // Reset the index
+        sendDataIndex = 0
+        
+        // save central
+        connectedCentral = central
+        
+        // Start sending
+        sendData()
+    }
+    
+    /*
+     *  Recognize when the central unsubscribes
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, central: CBCentral, didUnsubscribeFrom characteristic: CBCharacteristic) {
+        print("Central unsubscribed from characteristic")
+        connectedCentral = nil
+    }
+    
+    /*
+     *  This callback comes in when the PeripheralManager is ready to send the next chunk of data.
+     *  This is to ensure that packets will arrive in the order they are sent
+     */
+    func peripheralManagerIsReady(toUpdateSubscribers peripheral: CBPeripheralManager) {
+        // Start sending again
+        sendData()
+    }
+    
+    /*
+     * This callback comes in when the PeripheralManager received write to characteristics
+     */
+    func peripheralManager(_ peripheral: CBPeripheralManager, didReceiveWrite requests: [CBATTRequest]) {
+        for aRequest in requests {
+            guard let requestValue = aRequest.value,
+                let stringFromData = String(data: requestValue, encoding: .utf8) else {
+                    continue
+            }
+            
+            print("Received write request of %d bytes: %s", requestValue.count, stringFromData)
+            print(stringFromData)
+        }
+    }
+}
+
+let myPeripheral = Peripheral.init()
+RunLoop.main.run()

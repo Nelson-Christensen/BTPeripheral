ba68e48156204a2fb5462249af8bd7c92e8e6efa
Tobias <37023795+Forsenen@users.noreply.github.com>
2022-09-30 21:43:23 +0200

    sound change
    changed sound to easy change

--- Sources/BTPeripheral/main.swift
+++ Sources/BTPeripheral/main.swift
@@ -9,9 +9,11 @@ import Foundation
 import CoreBluetooth
 import AppKit
 import PythonKit
+
 PythonLibrary.useVersion(3)
 print("Python version ")
 print(Python.version)
+
 
 struct TransferService {
     static let serviceUUID = CBUUID(string: "1F2AD508-3BD6-485F-A2B1-F96ADBFB93E5")
@@ -29,6 +31,43 @@ var dirDestination = "asset_temp"
 var stringToWrite = ""
 var stringHDName = ""
 var mute = 0
+
+public extension NSSound {
+    static let basso     = NSSound(named: .basso)
+    static let blow      = NSSound(named: .blow)
+    static let bottle    = NSSound(named: .bottle)
+    static let frog      = NSSound(named: .frog)
+    static let funk      = NSSound(named: .funk)
+    static let glass     = NSSound(named: .glass)
+    static let hero      = NSSound(named: .hero)
+    static let morse     = NSSound(named: .morse)
+    static let ping      = NSSound(named: .ping)
+    static let pop       = NSSound(named: .pop)
+    static let purr      = NSSound(named: .purr)
+    static let sosumi    = NSSound(named: .sosumi)
+    static let submarine = NSSound(named: .submarine)
+    static let tink      = NSSound(named: .tink)
+}
+
+
+
+public extension NSSound.Name {
+    static let basso     = NSSound.Name("Basso")
+    static let blow      = NSSound.Name("Blow")
+    static let bottle    = NSSound.Name("Bottle")
+    static let frog      = NSSound.Name("Frog")
+    static let funk      = NSSound.Name("Funk")
+    static let glass     = NSSound.Name("Glass")
+    static let hero      = NSSound.Name("Hero")
+    static let morse     = NSSound.Name("Morse")
+    static let ping      = NSSound.Name("Ping")
+    static let pop       = NSSound.Name("Pop")
+    static let purr      = NSSound.Name("Purr")
+    static let sosumi    = NSSound.Name("Sosumi")
+    static let submarine = NSSound.Name("Submarine")
+    static let tink      = NSSound.Name("Tink")
+}
+
 
 func runPythonCode(){
     guard let url = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first else {
@@ -360,7 +399,9 @@ concurrentQueue.async {
                                 do{
                                     try FileManager.default.moveItem(atPath: oldFile, toPath: dirPath)
                                     if mute == 0{
-                                        NSSound(named: "Sonar")?.play()
+                                        NSSound.submarine?.stop()
+                                        NSSound.submarine?.play()
+                                        
                                     }
                                     
                                 } catch   {

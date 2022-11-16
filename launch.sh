663ffe132a39d237bc6e8a458759c3a9bb0502c4
Tobias <37023795+Forsenen@users.noreply.github.com>
2022-11-16 12:04:26 +0100

    Update launch.sh

--- launch.sh
+++ launch.sh
@@ -5,6 +5,8 @@ export PATH=$PATH:/bin
 HTTP_URL="www.creekside.se/download/mk3/"
 VERSION_URL="version.json"
 VERSION_OUTPUT="--output ./version_server.json"
+MD5_URL="md5sum.md5"
+MD5_OUTPUT="--output ./md5sum_server.md5"
 APP_URL="BTPeripheral.zip"
 APP_OUTPUT="--output ./BTPeripheral.zip"
 CURL_CMD="curl -w %{http_code}"
@@ -21,21 +23,32 @@ else
         if [[ ${CURL_OUTPUT} -ne 200 ]]; then
             echo "Curl connection failed with return code - ${CURL_OUTPUT}"
         else
-            echo "Remove old files"
-            rm -r ./mk3_macmini
-            echo "Unpack new files"
-            unzip -q ./mk3_macmini.zip
-            echo "Remove zip file"
-            rm ./mk3_macmini.zip
-            echo "Update Version"
-            #cp ./version_server.json ./version.json
+            md5 -q ./BTPeripheral.zip > ./md5sum.md5
+            CURL_OUTPUT=`${CURL_CMD} ${CURL_MAX_CONNECTION_TIMEOUT} ${HTTP_URL}${MD5_URL} ${MD5_OUTPUT}`
+            if [[ ${CURL_OUTPUT} -ne 200 ]]; then
+                echo "Curl connection failed with return code - ${CURL_OUTPUT}"
+            else
+                if cmp ./md5sum.md5 ./md5sum_server.md5 | grep "differ"; then
+                    echo "md5sum missmatch"
+                else
+                    echo "md5sum correct"
+                    echo "Remove old files"
+                    rm -rf ./BTPeripheral
+                    echo "Unpack new files"
+                    unzip -q ./BTPeripheral.zip
+                    echo "Remove zip file"
+                    rm ./BTPeripheral.zip
+                    echo "Update Version"
+                    #cp ./version_server.json ./version.json
+                fi
+            fi
         fi
     else
         echo "Up to date"
     fi
 fi
 
-cd ./mk3_macmini
+cd ./BTPeripheral
 
 echo waiting 1s before attempting to start
 sleep 1

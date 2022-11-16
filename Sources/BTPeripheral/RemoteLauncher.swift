6658ee83f03dde5d7c3aa6cab5f4f4698689f4f3
Tobias <37023795+Forsenen@users.noreply.github.com>
2022-11-16 14:41:20 +0100

    Update RemoteLauncher.swift

--- Sources/BTPeripheral/RemoteLauncher.swift
+++ Sources/BTPeripheral/RemoteLauncher.swift
@@ -28,7 +28,7 @@ func safeShell(_ command: String) throws -> String {
 func LaunchRemote()
 {
     do {
-        try safeShell("/usr/bin/automator ~/Documents/GitHub/Automater/StartRemote.app")
+        try safeShell("/usr/bin/automator ~/Documents/GitHub/StartRemote.app")
     }
     catch {
         print("\(error)") //handle or silence the error here
@@ -38,7 +38,7 @@ func LaunchRemote()
 func QuitRemote()
 {
     do {
-        try safeShell("/usr/bin/automator ~/Documents/GitHub/Automater/QuitRemote.app")
+        try safeShell("/usr/bin/automator ~/Documents/GitHub/QuitRemote.app")
     }
     catch {
         print("\(error)") //handle or silence the error here
